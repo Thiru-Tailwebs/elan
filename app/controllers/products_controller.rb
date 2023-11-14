fa371c64d89590f992aec2918d918a247e020a89
kartik <kartik@tailwebs.com>
2023-11-14 18:54:29 +0530

    Products landing page done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,8 @@
 class ProductsController < ApplicationController
   def index
     @categories = Category.where(active: true).order(:priority)
+    @loop_size = 3
+    @num_batches = (@categories.count / @loop_size.to_f).ceil
   end
 
   def show
