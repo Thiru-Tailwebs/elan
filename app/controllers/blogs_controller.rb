82f6d1245d807398c047a24148252b36287e0ec6
kartik <kartik@tailwebs.com>
2023-11-15 18:46:32 +0530

    Updated blogs page.

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -3,11 +3,17 @@ class BlogsController < ApplicationController
     @blogs_of_3 = Blog.all_published_data.order('published_dt DESC').first(3)
     @all_blogs = Blog.all_published_data.where.not(
       id: @blogs_of_3.pluck(:id)
-    ).order('published_dt DESC').first(3)
-    puts "===> @all_blogs: ", @all_blogs.count
+    ).order('published_dt DESC')
+    @loop_size = 2
+    @num_batches = (@all_blogs.count / @loop_size.to_f).ceil
   end
 
   def show
     @blog = Blog.friendly.find(params[:id])
+    @all_blogs = Blog.all_published_data.where.not(
+      id: @blog.id
+    ).order('published_dt DESC')
+    @loop_size = 2
+    @num_batches = (@all_blogs.count / @loop_size.to_f).ceil
   end
 end
