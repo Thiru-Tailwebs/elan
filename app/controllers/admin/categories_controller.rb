ae299c3e6fd6f79563f1fd4c34ed81874f5affe3
kartik <kartik@tailwebs.com>
2023-11-16 18:00:18 +0530

    Update CategoriesController in admin.

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -73,7 +73,7 @@ class Admin::CategoriesController < Admin::AdminbaseController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_category
-      @category = Category.find(params[:id])
+      @category = Category.friendly.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
